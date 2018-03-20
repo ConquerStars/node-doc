95fca633409e91c49d4879ca36e64cec7d6ff05e
ConquerStars <503453191@qq.com>
2018-03-20 23:56:49 +0800

    edit

--- static/util/routerCtr.js
+++ static/util/routerCtr.js
@@ -1,7 +1,7 @@
 const docRouter = {
-    orignPath: 'E:/daheng/markdown-docs/',
+    orignPath: 'F:/nodejs/markdown-doc/',
     // orignPath:'/home/wwwroot/markdown-docs/',
-    orignUrl: 'http://10.168.1.211',
+    orignUrl: 'http://192.168.1.65',
     // orignUrl: 'http://web2.daheng.co',
     listenPort: 3000,
 }
